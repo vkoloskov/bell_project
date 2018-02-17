fce6ac7b23b60ba315964406aac37136b3ec8854
Васин Кирилл Романович <Vasin.K.R@sberbank.ru>
2018-02-17 13:13:33 +0300

    case-insensitive sql scheme

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS `Person` (
-    `id`         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    `version`    INTEGER NOT NULL,
-    `first_name` VARCHAR(50) NOT NULL,
-    `house_id`   INTEGER,
-    `age`        INTEGER  NOT NULL
+CREATE TABLE IF NOT EXISTS Person (
+    id         INTEGER  PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    house_id   INTEGER,
+    age        INTEGER  NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `House` (
-    `id`         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    `version`    INTEGER NOT NULL,
-    `address`    VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS House (
+    id         INTEGER  PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER NOT NULL,
+    address    VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE `Person` ADD FOREIGN KEY (`house_id`) REFERENCES `House`(`id`);
+CREATE INDEX IX_Person_House_Id ON Person (house_id);
+ALTER TABLE Person ADD FOREIGN KEY (house_id) REFERENCES House(id);
