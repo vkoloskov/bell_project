4c9a1b0e858f1030e6870974075b7805a91f668a
Васин Кирилл Романович <Vasin.K.R@sberbank.ru>
2018-04-08 19:30:14 +0300

    request mappings

--- src/main/java/ru/bellintegrator/practice/person/controller/PersonController.java
+++ src/main/java/ru/bellintegrator/practice/person/controller/PersonController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,13 +34,13 @@ public class PersonController {
             @ApiResponse(code = 200, message = "Success", response = String.class),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")})
-    @RequestMapping(value = "/person", method = {POST})
+    @PostMapping("/person")
     public void person(@RequestBody PersonView person) {
         personService.add(person);
     }
 
     @ApiOperation(value = "getPersons", nickname = "getPersons", httpMethod = "GET")
-    @RequestMapping(value = "/person", method = {GET})
+    @GetMapping("/person")
     public List<PersonView> persons() {
         return personService.persons();
     }
