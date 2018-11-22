cbd9a816b360cd67d9e3646b07641814bd701fe6
Васин Кирилл Романович <Vasin.K.R@sberbank.ru>
2018-11-22 10:48:02 +0300

    Исправлена стратегия генерации первичного ключа

--- src/main/java/ru/bellintegrator/practice/model/House.java
+++ src/main/java/ru/bellintegrator/practice/model/House.java
@@ -3,6 +3,7 @@ package ru.bellintegrator.practice.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Version;
@@ -16,7 +17,7 @@ import java.util.Set;
 public class House {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
     private Long id;
 
