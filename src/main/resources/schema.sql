66cfa52cd2bb3062e7643b7d8e4bf93718978890
VKoloskov <JSbk3Z9D_1>
2021-11-29 19:42:33 +0300

    add schema and data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,34 +1,3 @@
-CREATE TABLE IF NOT EXISTS Person (
-                                      id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-                                      version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-                                      first_name VARCHAR(50) NOT NULL COMMENT 'Имя',
-    age        INTEGER  NOT NULL    COMMENT 'Возраст'
-    );
-COMMENT ON TABLE Person IS 'Человек';
-
-CREATE TABLE IF NOT EXISTS House (
-                                     id         INTEGER              COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
-                                     version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-                                     address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
-    );
-COMMENT ON TABLE House IS 'Дом';
-
-CREATE TABLE IF NOT EXISTS Person_House (
-                                            person_id   INTEGER  NOT NULL COMMENT 'Уникальный идентификатор человека',
-                                            house_id    INTEGER  NOT NULL COMMENT 'Уникальный идентификатор дома',
-
-                                            PRIMARY KEY (person_id, house_id)
-    );
-COMMENT ON TABLE Person_House IS 'join-таблица для связи человека и дома';
-
-CREATE INDEX IX_Person_House_Id ON Person_House (house_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (house_id) REFERENCES House(id);
-
-CREATE INDEX IX_House_Person_Id ON Person_House (person_id);
-ALTER TABLE Person_House ADD FOREIGN KEY (person_id) REFERENCES Person(id);
-
-
-
 CREATE TABLE IF NOT EXISTS Organization
 (
     id        INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
@@ -53,27 +22,15 @@ CREATE INDEX Office_id on Office(id);
 
 CREATE TABLE IF NOT EXISTS Citizenship
 (
-    code INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY,
+    code VARCHAR(50) COMMENT 'Уникальный идентификатор' PRIMARY KEY,
     name VARCHAR(50) NOT NULL COMMENT 'Название'
     );
 
 CREATE TABLE IF NOT EXISTS DocType
 (
-    code INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY,
+    code VARCHAR(50) COMMENT 'Уникальный идентификатор' PRIMARY KEY,
     name VARCHAR(50) NOT NULL COMMENT 'Имя'
 );
-
-CREATE TABLE IF NOT EXISTS Document
-(
-    id         INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
-    doc_name   VARCHAR(50) NOT NULL COMMENT 'Имя',
-    doc_number VARCHAR(50) NOT NULL COMMENT 'Номер',
-    doc_date   VARCHAR(50) NOT NULL COMMENT 'Отчество',
-    doc_code   INTEGER     NOT NULL COMMENT 'Код документа',
-    FOREIGN KEY (doc_code) REFERENCES DocType (code),
-);
-CREATE INDEX Document_id ON Document(id);
 
 CREATE TABLE IF NOT EXISTS User
 (
@@ -85,14 +42,26 @@ CREATE TABLE IF NOT EXISTS User
     position         VARCHAR(50) NOT NULL COMMENT 'Должность',
     phone            VARCHAR(50) NOT NULL COMMENT 'Телефон',
     is_identified    BOOLEAN COMMENT 'Идентифицирован?',
-    citizenship_code INTEGER     NOT NULL COMMENT 'Гражданство',
+    citizenship_code VARCHAR(50) NOT NULL COMMENT 'Гражданство',
     office_id        INTEGER     NOT NULL,
-    document_id        INTEGER     NOT NULL,
     FOREIGN KEY (office_id) REFERENCES Office (id),
     FOREIGN KEY (citizenship_code) REFERENCES Citizenship (code),
-    FOREIGN KEY (document_id) REFERENCES Document (id)
     );
 CREATE INDEX User_id ON User(id);
+
+CREATE TABLE IF NOT EXISTS Document
+(
+    id         INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version    INTEGER NOT NULL     COMMENT 'Служебное поле hibernate',
+    doc_name   VARCHAR(50) NOT NULL COMMENT 'Имя',
+    doc_number VARCHAR(50) NOT NULL COMMENT 'Номер',
+    doc_date   VARCHAR(50) NOT NULL COMMENT 'Отчество',
+    doc_code   VARCHAR(50) NOT NULL COMMENT 'Код документа',
+    user_id        INTEGER     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User (id),
+    FOREIGN KEY (doc_code) REFERENCES DocType (code),
+);
+CREATE INDEX Document_id ON Document(id);
 
 
 
